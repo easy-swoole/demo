6d095ea473c500dc5f990dfa55f6fa817f0d9f2f
如果的如果 <291323003@qq.com>
2018-03-10 22:36:23 +0800

    提交

--- Application/HttpController/Pool/Index.php
+++ Application/HttpController/Pool/Index.php
@@ -10,6 +10,7 @@ namespace App\HttpController\Pool;
 
 
 use App\Utility\MysqlPool;
+use App\Utility\MysqlPool2;
 use EasySwoole\Core\Http\AbstractInterface\Controller;
 
 class Index extends Controller
@@ -18,28 +19,72 @@ class Index extends Controller
     function index()
     {
         // TODO: Implement index() method.
-        var_dump(time());
-        \go(function () {
+        \go(function (){
             $db = MysqlPool::getInstance()->getObj();
+            \go(function (){
+                $db = MysqlPool::getInstance()->getObj();
+                if($db){
+                    $db->query('select sleep(1)');
+                    MysqlPool::getInstance()->freeObj($db);
+                }else{
+                    var_dump('db not available');
+                }
+                var_dump('finish at'.time());
+            });
             if($db){
                 $db->query('select sleep(1)');
-                var_dump('exec query at '.time());
                 MysqlPool::getInstance()->freeObj($db);
             }else{
-                var_dump('mysql pool not available');
+                var_dump('db not available');
             }
+            var_dump('finish at'.time());
         });
-        \go(function () {
-            $db = MysqlPool::getInstance()->getObj();
-            if($db){
-                $db->query('select sleep(1)');
-                var_dump('exec query at '.time());
-                MysqlPool::getInstance()->freeObj($db);
-            }else{
-                var_dump('mysql pool not available');
-            }
-        });
-        var_dump(time());
+
         $this->response()->write('request over');
+    }
+
+    function test()
+    {
+        \go(function (){
+            $db = MysqlPool2::getInstance()->getObj();
+            if($db){
+                $ret = $db->where('account','%s%','LIKE')->get('user_list');
+                MysqlPool2::getInstance()->freeObj($db);
+                var_dump($ret);
+            }else{
+                var_dump('db not available');
+            }
+        });
+
+        \go(function (){
+            $db = MysqlPool2::getInstance()->getObj();
+            if($db){
+                $ret = $db->where('account','test2')->get('user_list');
+                MysqlPool2::getInstance()->freeObj($db);
+                var_dump($ret);
+            }else{
+                var_dump('db not available');
+            }
+        });
+
+
+        $this->response()->write('request over');
+    }
+
+
+    function test2()
+    {
+        //协程同步调用（优化worker 利用时间，让一个worker可以同时处理多个用户请求）
+        $ret = null;
+        $db = MysqlPool2::getInstance()->getObj();
+
+        if($db){
+            var_dump($db->get('user_list'));
+            MysqlPool2::getInstance()->freeObj($db);
+        }else{
+            var_dump('db not available');
+        }
+
+        $this->response()->write(json_encode($ret));
     }
 }
