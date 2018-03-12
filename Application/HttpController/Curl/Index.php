488e015677ae79f636ffa83315f51d40c2b48343
如果的如果 <291323003@qq.com>
2018-03-12 14:21:33 +0800

    提交

--- Application/HttpController/Curl/Index.php
+++ Application/HttpController/Curl/Index.php
@@ -10,6 +10,7 @@ namespace App\HttpController\Curl;
 
 
 use EasySwoole\Core\Http\AbstractInterface\Controller;
+use EasySwoole\Core\Swoole\Coroutine\Client\Http;
 use EasySwoole\Core\Utility\Curl\Request;
 use EasySwoole\Core\Utility\Random;
 
@@ -39,4 +40,54 @@ class Index extends Controller
         var_dump($content);
         $this->response()->write('exec success');
     }
+
+
+
+    function sleep()
+    {
+        $time = intval($this->request()->getRequestParam('time'));
+
+        usleep($time*100000);
+
+        $this->response()->write("sleep {$time}");
+    }
+
+
+    function concurrent()
+    {
+        //以下流程网络IO的时间就接近于 MAX(q1网络IO时间, q2网络IO时间)。
+        $micro = microtime(true);
+        $q1 = new Http('http://127.0.0.1:9501/curl/sleep/index.html?time=1');
+        $c1 = $q1->exec(true);
+
+        $q2 = new Http('http://127.0.0.1:9501/curl/sleep/index.html?time=4');
+        $c2 = $q2->exec(true);
+
+        $c1->recv();
+        $c1->close();
+        $c2->recv();
+        $c2->close();
+
+        var_dump($c1->body);
+        var_dump($c2->body);
+
+        $time = round(microtime(true) - $micro,3);
+        $this->response()->write($time);
+
+    }
+
+    function noConcurrent()
+    {
+        //传统阻塞
+        $micro = microtime(true);
+        $req = new Request('http://127.0.0.1:9501/curl/sleep/index.html?time=1');
+        var_dump($req->exec()->getBody());
+
+        $req2 = new Request('http://127.0.0.1:9501/curl/sleep/index.html?time=4');
+        var_dump($req2->exec()->getBody());
+
+        $time = round(microtime(true) - $micro,3);
+        $this->response()->write($time);
+    }
+
 }
