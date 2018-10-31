144f79df5026a524fafc1c3007c7b9f46201ed59
Tioncico <31308307+tioncico@users.noreply.github.com>
2018-10-31 17:08:29 +0800

    增加beforeUse
    增加beforeUse

--- Application/Utility/Pool/MysqlPoolObj.php
+++ Application/Utility/Pool/MysqlPoolObj.php
@@ -33,4 +33,14 @@ class MysqlPoolObj extends Mysqli implements PoolObjectInterface
         $this->rollback();
         $this->resetDbStatus();
     }
+
+    /**
+     * @return bool
+     */
+    function beforeUse(): bool
+    {
+        //使用前调用,当返回true，表示该对象可用。返回false，该对象失效，需要回收
+        return true;
+    }
+
 }
