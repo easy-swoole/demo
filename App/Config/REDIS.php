3ce38facf0349e8c475dd9c6d1469fe6163e808a
tioncico <1067197739@qq.com>
2018-11-01 14:36:03 +0800

    demo update

---
+++ App/Config/REDIS.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Apple
+ * Date: 2018/10/30 0030
+ * Time: 16:11
+ */
+
+return [
+    'host'       => '127.0.0.1',
+    'port'       => 6379,
+    'password'   => '',
+    'select'     => 0,
+    'timeout'    => 0,
+    'expire'     => 0,
+    'persistent' => false,
+    'prefix'     => '',
+];
