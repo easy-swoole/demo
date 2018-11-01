ab53112ee5f6dfc6eb843436ed748d1263bef5df
1161709455@qq.com <h8643160@>
2018-11-01 14:23:32 +0800

    数据库连接池例子

---
+++ App/Model/User/UserModel.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 18-11-1
+ * Time: 下午1:51
+ */
+
+namespace App\Model\User;
+
+
+use App\Model\BaseModel;
+
+class UserModel extends BaseModel
+{
+    protected $table = 'user';
+
+    /*
+     * 获取列表数据
+     */
+    function getAll(int $page = 1, int $pageSize = 10) {
+        $data = $this->getDbConnection()->withTotalCount()->orderBy('id', 'DESC')->get($this->table, [($page - 1) * $pageSize, $page * $pageSize]);
+        $total = $this->getDbConnection()->getTotalCount();
+        return ['data' => $data, 'total' => $total];
+    }
+
+    /*
+     * 获取用户详情
+     */
+    function getOne(UserBean $userBean):?UserBean {
+        $data = $this->getDbConnection()->where('id', $userBean->getId())->getOne($this->table);
+        return empty($data) ? null : new UserBean($data);
+    }
+}
