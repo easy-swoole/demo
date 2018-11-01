5c666d2f78bc5c1585b5d1576b05935b35772a23
1161709455@qq.com <h8643160@>
2018-11-01 15:42:36 +0800

    工具类脚本例子

---
+++ App/Tools/random.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: root
+ * Date: 18-11-1
+ * Time: 下午3:32
+ */
+
+// 加载库文件
+require_once __DIR__."/../../vendor/autoload.php";
+
+// 随机生成4位字符串
+$str = \EasySwoole\Utility\Random::character(4);
+var_dump($str);
+
+// 随机生成4位数字字符串
+$number = \EasySwoole\Utility\Random::number(6);
+var_dump($number);
+
+// 随机从数组里面获取一个元素
+$data = ['apple', 'orange', 'pear', 'grape'];
+var_dump(\EasySwoole\Utility\Random::arrayRandOne($data));
