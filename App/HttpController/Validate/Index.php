a86700826060f63dad13469ce7835568d5230355
1161709455@qq.com <h8643160@>
2018-11-14 16:41:30 +0800

    控制器验证

--- App/HttpController/Validate/Index.php
+++ App/HttpController/Validate/Index.php
@@ -18,7 +18,7 @@ class Index extends Base
     function index() {
         $validate = new Validate();
         $validate->addColumn('name')->required('姓名必填');
-        $validate->addColumn('age')->required('年龄必填')->between(20, 30, '年轻只能在20岁到30岁之前');
+        $validate->addColumn('age')->required('年龄必填')->between(20, 30, '年龄只能在20岁到30岁之前');
         if ($this->validate($validate)) {
             $this->writeJson(Status::CODE_OK, null, 'success');
         } else {
