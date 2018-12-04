3a626688b91c27be461674c69a8a234ef20e5d7d
tioncico <1067197739@qq.com>
2018-12-04 10:50:38 +0800

    更新异步任务模板

--- App/HttpController/Task.php
+++ App/HttpController/Task.php
@@ -9,14 +9,18 @@
 namespace App\HttpController;
 
 
+use App\Task\QuickTaskTest;
 use App\Task\TaskTest;
 use EasySwoole\EasySwoole\Console\TcpService;
 use EasySwoole\EasySwoole\Swoole\Task\TaskManager;
 use EasySwoole\Http\AbstractInterface\Controller;
+use EasySwoole\Spl\SplBean;
 use Swoole\Process;
 
 class Task extends Controller
 {
+    protected $a=1;
+    static $b;
     function index()
     {
         $this->response()->write("task");
@@ -25,16 +29,21 @@ class Task extends Controller
 //        });
 //        var_dump($result);
         $result = TaskManager::async(new TaskTest());
-//        var_dump($result);
+        $result = TaskManager::async(QuickTaskTest::class);
+        var_dump($result);
 //        $result = TaskManager::sync(function (){
 //            echo "执行task同步任务(回调函数)\n";
 //        });
 //        var_dump($result);
-        $result = TaskManager::sync(new TaskTest());
-        var_dump($result);
+//        $result = TaskManager::sync(new TaskTest());
+//        var_dump($result);
 
 
         // TODO: Implement index() method.
+    }
+
+    function test(){
+
     }
 
 }
