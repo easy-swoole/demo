590b8e2a6acb9d5259ae876fa61e5afdfe5c7c79
tioncico <1067197739@qq.com>
2018-12-07 15:33:01 +0800

    更新rpc文档

--- App/Rpc/CliTest.php
+++ App/Rpc/CliTest.php
@@ -1,5 +1,5 @@
 <?php
-include 'vendor/autoload.php';
+include '../../vendor/autoload.php';
 $conf = new \EasySwoole\Rpc\Config();
 //开启通讯密钥
 //$conf->setAuthKey('123456');
@@ -7,7 +7,7 @@ $rpc = new \EasySwoole\Rpc\Rpc($conf);
 
 //虚拟一个服务节点
 $serviceNode = new \EasySwoole\Rpc\ServiceNode();
-$serviceNode->setServiceName('serviceName');
+$serviceNode->setServiceName('ServiceOne');
 $serviceNode->setServiceIp('127.0.0.1');
 $serviceNode->setServicePort(9601);
 $serviceNode->setNodeId('qwe');
@@ -17,7 +17,7 @@ $rpc->nodeManager()->refreshServiceNode($serviceNode);
 
 go(function () use ($rpc) {
     $client = $rpc->client();
-    $client->selectService('serviceOne')->callAction('a1')->setArg(
+    $client->selectService('ServiceOne')->callAction('a1')->setArg(
         [
             'callTime' => time()
         ]
@@ -27,7 +27,7 @@ go(function () use ($rpc) {
         var_dump('fail' . $response->getStatus());
     })->setTimeout(1.5);
 
-    $client->selectService('serviceName')->callAction('a2')->onSuccess(function (\EasySwoole\Rpc\Task $task, \EasySwoole\Rpc\Response $response, ?\EasySwoole\Rpc\ServiceNode $serviceNode) {
+    $client->selectService('ServiceOne')->callAction('a2')->onSuccess(function (\EasySwoole\Rpc\Task $task, \EasySwoole\Rpc\Response $response, ?\EasySwoole\Rpc\ServiceNode $serviceNode) {
         var_dump('success' . $response->getMessage());
     });
     $client->call(1.5);
