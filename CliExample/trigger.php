7b2e6f2721ff7ae7acf7e1e011d0de6cbd851494
1161709455@qq.com <h8643160@>
2018-12-11 10:20:44 +0800

    触发异常

--- CliExample/trigger.php
+++ CliExample/trigger.php
@@ -8,10 +8,11 @@
 
 require_once __DIR__."/../vendor/autoload.php";
 
-$l = new \EasySwoole\Trace\Bean\Location();
 $fruit = ['apple', 'orange', 'banana'];
+$l = new \EasySwoole\Trace\Bean\Location();
 $l->setFile(__FILE__);
 $l->setLine(10);
 \EasySwoole\EasySwoole\Trigger::getInstance()->error('Undefined index: key', $l);
+\EasySwoole\EasySwoole\Trigger::getInstance()->throwable(new \Exception("hello easyswoole"));
 var_dump($fruit);
 
