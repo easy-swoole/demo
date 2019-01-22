a5c497706d4744f56ff5891b6ffc85b8eedec4ba
1161709455@qq.com <h8643160@>
2019-01-22 10:16:15 +0800

    log

--- App/Log/MyLogHandle.php
+++ App/Log/MyLogHandle.php
@@ -13,12 +13,14 @@ class MyLogHandle implements LoggerInterface {
     public function console(string $str, $category = null, $saveLog = true): ?string
     {
         // TODO: Implement console() method.
+        echo "这是自定义的log处理,输出:$str\n";
         return "这是自定义的log处理,输出:$str\n";
     }
 
     public function log(string $str, $logCategory = null, int $timestamp = null): ?string
     {
         // TODO: Implement log() method.
+        echo "这是自定义的log处理,模拟写入:[$logCategory]$str\n";
         return "这是自定义的log处理,模拟写入:[$logCategory]$str\n";
     }
 }
