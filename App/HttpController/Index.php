a274ac726c702080186b0fad21ff5fc80a47625e
yf <291323003@qq.com>
2019-03-05 20:57:35 +0800

    up

---
+++ App/HttpController/Index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yf
+ * Date: 2019-03-05
+ * Time: 20:51
+ */
+
+namespace App\HttpController;
+
+
+use EasySwoole\Http\AbstractInterface\Controller;
+
+class Index extends Controller
+{
+
+    function index()
+    {
+        // TODO: Implement index() method.
+    }
+}
