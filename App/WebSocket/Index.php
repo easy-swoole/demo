96331dfc1ee6f3e5d91e0c90ae8a69e3b56ca436
RunsTp <runstp@163.com>
2019-03-11 12:04:15 +0800

    :sparkles: 完成 websocket 测试类

--- App/WebSocket/Index.php
+++ App/WebSocket/Index.php
@@ -11,7 +11,14 @@ use EasySwoole\EasySwoole\ServerManager;
 use EasySwoole\EasySwoole\Swoole\Task\TaskManager;
 use EasySwoole\Socket\AbstractInterface\Controller;
 
-class Test extends Controller
+/**
+ * Class Index
+ *
+ * 此类是默认的 websocket 消息解析后访问的 控制器
+ *
+ * @package App\WebSocket
+ */
+class Index extends Controller
 {
     function hello()
     {
@@ -28,13 +35,12 @@ class Test extends Controller
         $client = $this->caller()->getClient();
 
         // 异步推送, 这里直接 use fd也是可以的
-        // TaskManager::async 回调参数中的代码是在 task 进程中执行的 默认不含连接池 需要注意可能出现 getPool null的情况
         TaskManager::async(function () use ($client){
             $server = ServerManager::getInstance()->getSwooleServer();
             $i = 0;
             while ($i < 5) {
                 sleep(1);
-                $server->push($client->getFd(),'push in http at '.time());
+                $server->push($client->getFd(),'push in http at '. date('H:i:s'));
                 $i++;
             }
         });
