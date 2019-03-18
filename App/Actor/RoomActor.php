4737b18ac59f00f0b896288a10e7304566e1fbd4
yf <291323003@qq.com>
2019-03-18 22:00:50 +0800

    up

---
+++ App/Actor/RoomActor.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yf
+ * Date: 2019-03-18
+ * Time: 22:00
+ */
+
+namespace App\Actor;
+
+
+use EasySwoole\Actor\AbstractActor;
+use EasySwoole\Actor\ActorConfig;
+
+class RoomActor extends AbstractActor
+{
+
+    static function configure(ActorConfig $actorConfig)
+    {
+        // TODO: Implement configure() method.
+        return 'room';
+    }
+
+    function onStart($arg)
+    {
+        // TODO: Implement onStart() method.
+    }
+
+    function onMessage($msg)
+    {
+        // TODO: Implement onMessage() method.
+    }
+
+    function onExit($arg)
+    {
+        // TODO: Implement onExit() method.
+    }
+
+    protected function onException(\Throwable $throwable)
+    {
+        // TODO: Implement onException() method.
+    }
+}
