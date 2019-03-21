b44b069bffc8f59c1e9b3e7fff3583f5e712d610
evalor <mipone@foxmail.com>
2019-03-21 15:46:25 +0800

    update

--- App/HttpController/Index.php
+++ App/HttpController/Index.php
@@ -17,9 +17,7 @@ class Index extends Controller
      */
     function index()
     {
-        $this->response()->withHeader('Content-type', 'text/html;charset=utf-8');
-        $this->response()->write('<div style="text-align: center;margin-top: 30px"><h2>欢迎使用EASYSWOOLE</h2></div></br>');
-        $this->response()->write('<div style="text-align: center">您现在看到的页面是默认的 Index 控制器的输出</div></br>');
-        $this->response()->write('<div style="text-align: center"><a href="https://www.easyswoole.com/Manual/2.x/Cn/_book/Base/http_controller.html">查看手册了解详细使用方法</a></div></br>');
+        $echostr = $this->request()->getRequestParam('echostr');
+        $this->response()->write($echostr);
     }
 }
