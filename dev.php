11c1e57d44d0fbc8c06ce3afbbaf8a7c609da6c4
yf <291323003@qq.com>
2019-06-09 19:56:10 +0800

    up

--- dev.php
+++ dev.php
@@ -23,16 +23,4 @@ return [
     ],
     'TEMP_DIR'      => null,//临时文件存放的目录
     'LOG_DIR'       => null,//日志文件存放的目录
-    'CONSOLE'       => [//console控制台组件配置
-        'ENABLE'         => true,//是否开启
-        'LISTEN_ADDRESS' => '127.0.0.1',//监听地址
-        'PORT'           => 9500,//监听端口
-        'USER'           => 'root',//验权用户名
-        'PASSWORD'       => '123456'//验权用户名
-    ],
-    'FAST_CACHE'    => [//fastCache组件
-        'PROCESS_NUM' => 0,//进程数,大于0才开启
-        'BACKLOG'     => 256,//数据队列缓冲区大小
-    ],
-    'DISPLAY_ERROR' => true,//是否开启错误显示
 ];
