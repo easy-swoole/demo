4ec7f8cfee113e4765ada4589c1e06ddb9fee371
huanglonghui <huanglonghui@loongcheer.com>
2020-12-31 10:52:43 +0800

    适配最新版本3.4.x

--- EasySwooleEvent.php
+++ EasySwooleEvent.php
@@ -9,6 +9,7 @@
 namespace EasySwoole\EasySwoole;
 
 
+use EasySwoole\Component\Di;
 use EasySwoole\EasySwoole\Swoole\EventRegister;
 use EasySwoole\EasySwoole\AbstractInterface\Event;
 use EasySwoole\Http\Request;
@@ -22,6 +23,17 @@ class EasySwooleEvent implements Event
     public static function initialize()
     {
         date_default_timezone_set('Asia/Shanghai');
+
+        // 使用 DI 注册 onRequest 全局事件
+        Di::getInstance()->set(SysConst::HTTP_GLOBAL_ON_REQUEST, function (Request $request, Response $response): bool {
+            // TODO: Implement onRequest() method.
+            return true;
+        });
+
+        // 使用 DI 注册 afterRequest 全局事件
+        Di::getInstance()->set(SysConst::HTTP_GLOBAL_AFTER_REQUEST, function (Request $request, Response $response): void {
+            // TODO: Implement afterRequest() method.
+        });
     }
 
     public static function mainServerCreate(EventRegister $register)
