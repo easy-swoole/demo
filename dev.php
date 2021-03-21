0852a6af6a4df5afc7939a180dc6901a52dcc65c
XueSi <1592328848@qq.com>
2021-03-21 16:43:59 +0800

    add demo: WebSocket and Http Service

--- dev.php
+++ dev.php
@@ -1,37 +1,33 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: yf
- * Date: 2019-01-01
- * Time: 20:06
- */
+
+use EasySwoole\Log\LoggerInterface;
 
 return [
     'SERVER_NAME' => "EasySwoole",
     'MAIN_SERVER' => [
         'LISTEN_ADDRESS' => '0.0.0.0',
         'PORT' => 9501,
-        'SERVER_TYPE' => EASYSWOOLE_WEB_SERVER, //可选为 EASYSWOOLE_SERVER  EASYSWOOLE_WEB_SERVER EASYSWOOLE_WEB_SOCKET_SERVER,EASYSWOOLE_REDIS_SERVER
+        'SERVER_TYPE' => EASYSWOOLE_WEB_SOCKET_SERVER, //可选为 EASYSWOOLE_SERVER  EASYSWOOLE_WEB_SERVER EASYSWOOLE_WEB_SOCKET_SERVER
         'SOCK_TYPE' => SWOOLE_TCP,
         'RUN_MODEL' => SWOOLE_PROCESS,
         'SETTING' => [
             'worker_num' => 8,
             'reload_async' => true,
-            'max_wait_time'=>3
+            'max_wait_time' => 3
         ],
-        'TASK'=>['workerNum'=>4,'maxRunningNum'=>128,'timeout'=>15]
+        'TASK' => [
+            'workerNum' => 4,
+            'maxRunningNum' => 128,
+            'timeout' => 15
+        ]
     ],
-    'TEMP_DIR' => null,
-    'LOG_DIR' => null,
-    'MYSQL'         => [
-        'host'          => '47.106.110.119',
-        'port'          => 3300,
-        'user'          => 'testEs',
-        'password'      => 'KdhHirAsRRY8WAaM',
-        'database'      => 'testEs',
-        'timeout'       => 5,
-        'charset'       => 'utf8mb4',
-        'POOL_MAX_NUM'  => 20,
-        'POOL_TIME_OUT' => 1
+    "LOG" => [
+        'dir' => null,
+        'level' => LoggerInterface::LOG_LEVEL_DEBUG,
+        'handler' => null,
+        'logConsole' => true,
+        'displayConsole'=>true,
+        'ignoreCategory' => []
     ],
+    'TEMP_DIR' => null
 ];
